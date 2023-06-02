5b35e633d32a837a5d2d31fc6f2da9a833f276c2
Janak <jaykapadiya8899@gmail.com>
2023-06-02 17:06:26 +0530

    updated globals css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-];
-export const theme = {
-  extend: {
-    colors: {
-      primary: "#1f2937",
-      "primary-dark": "#2c785c",
-      "regal-blue": "#97b1d1",
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1f2937",
+        "primary-dark": "#2c785c",
+        "regal-blue": "#97b1d1",
+      },
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
     },
   },
-  fontFamily: {
-    sans: ["Roboto", "sans-serif"],
-  },
+  plugins: [],
 };
-export const plugins = [];
