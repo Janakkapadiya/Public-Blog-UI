e28b086cb0be83c8f5090f6d23b03fbb346582c6
Janak <jaykapadiya8899@gmail.com>
2023-06-02 16:43:15 +0530

    debugin tailwindcc module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "../components/Footer";
-import "globals.css";
+import "./globals.css";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
