c2d6ec6f78897a7ed83d5be6cd7fe33120668956
janak <jaykapadiya8899@gmail.com>
2023-06-22 17:20:50 +0530

    added smp

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ export default async function sitemap() {
   const res = await fetch("https://urchin-app-4hcup.ondigitalocean.app/api");
   const allPosts = await res.json();
 
-  const posts = allPosts.map((post) => ({
+  const posts = allPosts.map((post: any) => ({
     url: `https://publicblog.live/${post.slug}`,
     lastModified: post.publishedAt,
   }));
